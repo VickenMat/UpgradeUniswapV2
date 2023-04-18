93dbe0272eb380175f3b9cd84915a102d3a897c9
Vicken Matiossian <113546737+VickenMat@users.noreply.github.com>
2023-04-18 17:48:31 -0400

    Tests

--- src/test/UniswapV2Pair.t.sol
+++ src/test/UniswapV2Pair.t.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import "../contracts/UniswapV2Pair.sol";
+
+contract TestUniswapV2Pair {
+    UniswapV2Pair pair;
+    address tokenA = 0x1234567890123456789012345678901234567890;
+    address tokenB = 0x0987654321098765432109876543210987654321;
+    uint112 reserveA = 1000;
+    uint112 reserveB = 2000;
+    uint32 blockTimestampLast = 0;
+
+    function beforeEach() public {
+        pair = new UniswapV2Pair();
+        pair.initialize(tokenA, tokenB);
+        pair.mint(address(this));
+    }
+
+    function testInitialize() public view {
+        assert(pair.token0() == tokenA);
+        assert(pair.token1() == tokenB);
+    }
+
+    function testMint() public view {
+        uint balance = pair.balanceOf(address(this));
+        assert(balance == 100);
+    }
+
+    // function testSwap() public {
+    //     pair.swap(0, 100, address(this), "");
+    //     uint balanceA = pair.getReserves().reserve0;
+    //     uint balanceB = pair.getReserves().reserve1;
+    //     assert(balanceA == 900);
+    //     assert(balanceB == 2100);
+    // }
+
+    function testBurn() public {
+        pair.burn(address(this));
+        uint balance = pair.balanceOf(address(this));
+        assert(balance == 0);
+    }
+
+    // function testSync() public {
+    //     pair.sync();
+    //     uint112 newReserveA = pair.getReserves().reserve0;
+    //     uint112 newReserveB = pair.getReserves().reserve1;
+    //     assert(newReserveA == reserveA);
+    //     assert(newReserveB == reserveB);
+    // }
+
+    function testGetReserves() public view {
+        (
+            uint112 _reserve0,
+            uint112 _reserve1,
+            uint32 _blockTimestampLast
+        ) = pair.getReserves();
+        assert(_reserve0 == reserveA);
+        assert(_reserve1 == reserveB);
+        assert(_blockTimestampLast == blockTimestampLast);
+    }
+}
--- src/test/UniswapV2Router02.t.sol
+++ src/test/UniswapV2Router02.t.sol
@@ -1,1 +1,98 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.19;
+
+// import "../contracts/UniswapV2Router02.sol";
+
+// contract TestUniswapV2Router02 {
+//     UniswapV2Router02 router;
+
+//     address private tokenA = 0x123;
+//     address private tokenB = 0x456;
+//     address private weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // address of WETH on mainnet
+
+//     address private owner = 0x789;
+
+//     uint256 private deadline = block.timestamp + 120; // 2 minute deadline
+
+//     uint256 private amountIn = 1000;
+//     uint256 private amountOutMin = 900;
+
+//     uint256 private ethValue = 1 ether; // 1 ETH
+
+//     function beforeEach() public {
+//         // Deploy UniswapV2Router02 contract
+//         router = new UniswapV2Router02(address(0), address(0));
+
+//         // Mint some WETH for testing
+//         IWETH(weth).deposit{value: ethValue}();
+//         assert(IWETH(weth).balanceOf(address(this)) == ethValue);
+
+//         // Approve WETH for router
+//         IWETH(weth).approve(address(router), ethValue);
+//     }
+
+//     function testSwapExactTokensForTokens() public {
+//         // Transfer some tokens to this contract for testing
+//         IERC20(tokenA).transferFrom(owner, address(this), amountIn);
+//         assert(IERC20(tokenA).balanceOf(address(this)) == amountIn);
+
+//         // Swap tokenA for tokenB
+//         uint256[] memory amounts = router.swapExactTokensForTokens(
+//             amountIn,
+//             amountOutMin,
+//             path,
+//             address(this),
+//             deadline
+//         );
+
+//         // Check output token amount
+//         assert(amounts[amounts.length - 1] >= amountOutMin);
+//     }
+
+//     function testSwapExactETHForTokens() public payable {
+//         // Swap ETH for tokenB
+//         uint256[] memory amounts = router.swapExactETHForTokens{
+//             value: ethValue
+//         }(amountOutMin, path, address(this), deadline);
+
+//         // Check output token amount
+//         assert(amounts[amounts.length - 1] >= amountOutMin);
+//     }
+
+//     function testSwapExactTokensForETH() public {
+//         // Transfer some tokens to this contract for testing
+//         IERC20(tokenA).transferFrom(owner, address(this), amountIn);
+//         assert(IERC20(tokenA).balanceOf(address(this)) == amountIn);
+
+//         // Swap tokenA for ETH
+//         uint256[] memory amounts = router.swapExactTokensForETH(
+//             amountIn,
+//             amountOutMin,
+//             path,
+//             address(this),
+//             deadline
+//         );
+
+//         // Check output ETH amount
+//         assert(amounts[amounts.length - 1] >= amountOutMin);
+
+//         // Check WETH balance of this contract
+//         assert(IWETH(weth).balanceOf(address(this)) >= ethValue);
+//     }
+
+//     function testSwapETHForExactTokens() public payable {
+//         // Swap ETH for tokenA
+//         uint256[] memory amounts = router.swapETHForExactTokens{
+//             value: ethValue
+//         }(amountIn, path, address(this), deadline);
+
+//         // Check input ETH amount
+//         assert(msg.value == amounts[0]);
+
+//         // Check output token amount
+//         assert(amounts[amounts.length - 1] == amountIn);
+
+//         // Check WETH balance of this contract
+//     }
+// }
